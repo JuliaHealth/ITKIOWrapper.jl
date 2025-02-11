46031fdd7161323567d0382762fe5c7c08c47362
divital-coder <divital2004@gmail.com>
2025-02-11 23:04:06 +0530

    [ITKIOWrapper-v1.0.0] Getting started

--- README.md
+++ README.md
@@ -6,25 +6,47 @@ Usage::
 using ITKIOWrapper
 ```
 
-Loading Spatial meta data from an image file path:
-
+## Loading Spatial meta data from an image file path:  
+> [!NOTE]
+> NIFTI
 ```
-spatialMeta = ITKIOWrapper.loadSpatialMetaData("filepath")
+nifti_image = load_image("./example_file.nii.gz")
+metadata = load_spatial_metadata(nifti_image)
 
-spatialMeta.origin
-
-spatialMeta.spacing
-
-spatialMeta.size
-
-spatialMeta.direction
-
+metadata.origin
+metadata.spacing
+metadata.size
+metadata.direction
 ```
 
-Loading Voxel data from an image file path and spatial meta data
+> [!NOTE]
+> DICOM
+```
+dicom_image = load_image("./example_dicom_series")
+metadata = load_spatial_metadata(dicom_image)
+
+metadata.origin
+metadata.spacing
+metadata.size
+metadata.direction
+```
+
+## Writing Images - Nifti To Nifti 
+```
+output_image("./source_file.nii.gz", "./output_new_example_file.nii.gz",false)
+```
+
+## Writing Images - Dicom To Nifti
+```
+output_image("./source_dicom_series", "./output_new_example_file.nii.gz", false)
+```
+
+## Loading Voxel data from an image file path and spatial meta data
 
 ```
-voxelData = ITKIOWrapper.loadVoxelData("filepath",spatialMeta)
+img = load_image("./sample.nii.gz")
+metadata = load_spatial_metadata(img)
+voxelData = load_voxel_data(img, metadata)
 voxelData.dat
 ```
 
--- src/ITKIOWrapper.jl
+++ src/ITKIOWrapper.jl
@@ -1,38 +1,39 @@
 module ITKIOWrapper
 using CxxWrap
 using ITKIOWrapper_jll
-
-export loadSpatialMetaData, loadVoxelData
-
+export load_spatial_metadata, load_voxel_data, load_image, output_image
 include("DataStructs.jl")
-
 
 @wrapmodule(()->ITKIOWrapper_jll.libITKIOWrapper_path)
 
 function __init__()
-@initcxx
+    @initcxx
 end
 
-function loadSpatialMetaData(filePath::String)
-
-  img = ITKImageWrapper(filePath, isdir(filePath)) #second argument is "isDicom"
-  ITKIOWrapper.reorientToLPS(img)
-  imgOrigin = Tuple(Float64.(getOrigin(img)))
-  imgSpacing = Tuple(Float64.(getSpacing(img)))
-  imgSize = Tuple(Int64.(getSize(img)))
-  imgDirection = NTuple{9,Float64}(map(Float64, getDirection(img)))
-
-  return DataStructs.SpatialMetaData(imgOrigin, imgSpacing, imgSize, imgDirection)
+function load_image(filePath::String)
+    return ITKImageWrapper(filePath, isdir(filePath))
 end
 
-function loadVoxelData(filePath::String, spatMeta::DataStructs.SpatialMetaData)
-  img = ITKImageWrapper(filePath, isdir(filePath))
-  ITKIOWrapper.reorientToLPS(img)
-  voxelData = reshape(getPixelData(img), spatMeta.size)
-  return DataStructs.VoxelData(voxelData)
+function output_image(srcFile::String, outputFilename::String, isDicomOutput::Bool=false)
+    img = ITKImageWrapper(src, isdir(src))
+    # Use the wrapped function directly
+    writeImage(img, outputFilename, isDicomOutput)
+end
+    
+function load_spatial_metadata(img::ITKImageWrapperAllocated)  # Note the type change
+    imgOrigin = Tuple(Float64.(getOrigin(img)))
+    imgSpacing = Tuple(Float64.(getSpacing(img)))
+    imgSize = Tuple(Int64.(getSize(img)))
+    imgDirection = NTuple{9,Float64}(map(Float64, getDirection(img)))
+    return DataStructs.SpatialMetaData(imgOrigin, imgSpacing, imgSize, imgDirection)
 end
 
+function load_voxel_data(img::ITKImageWrapperAllocated, spatMeta::DataStructs.SpatialMetaData)  # Note the type change
+    voxelData = reshape(getPixelData(img), spatMeta.size)
+    return DataStructs.VoxelData(voxelData)
+end
 
 greet() = print("Hello from ITKIOWrapper.jl!")
 
 end # module ITKIOWrapper
+
